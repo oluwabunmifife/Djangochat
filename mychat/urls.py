25515054e07cd6fce2b858d5c6c600a70e99416b
Oluwabunmi Fife <oluwabunmifife@gmail.com>
2022-05-13 22:27:43 +0100

    Made changes to room.html, views and urls. Project now fully works:)

--- mychat/urls.py
+++ mychat/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('<str:room>/', views.room, name='room'),
     path('checkview', views.checkview, name='checkview'),
     path('send', views.send, name='send'),
+    path('getMessages/<str:room>/', views.getMessages, name='getMessages'),
 ]
--- mychat/views.py
+++ mychat/views.py
@@ -2,7 +2,7 @@ from multiprocessing.sharedctypes import Value
 from unicodedata import name
 from django.shortcuts import render, redirect
 from .models import Room, Message
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
 
 # Create your views here.
 def home(request):
@@ -36,3 +36,9 @@ def send(request):
     new_message = Message.objects.create(value=message, user=username, room=room_id)
     new_message.save()
     return HttpResponse('Message sent succcesfully')
+
+def getMessages(request, room):
+    room_details = Room.objects.get(name=room)
+
+    messages = Message.objects.filter(room=room_details.id)
+    return JsonResponse({"messages":list(messages.values())})
